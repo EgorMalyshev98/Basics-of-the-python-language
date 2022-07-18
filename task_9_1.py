a251bbb18cd2233011cc11e18c7f0c4c16262644
EgorMalyshev98 <egor.malyshev9817@gmail.com>
2022-07-18 10:48:50 +0300

    lesson 9

---
+++ task_9_1.py
@@ -0,0 +1,17 @@
+import time
+
+
+class TrafficLight:
+    __color: str
+
+    def running(self):
+        dct = {'red': 4, 'yellow': 2, 'green': 3}
+        for col, sec in dct.items():
+            self.__color = col
+            print(self.__color, f'{sec} сек')
+            time.sleep(sec)
+
+
+if __name__ == '__main__':
+    traffic = TrafficLight()
+    traffic.running()
---
+++ task_9_5.py
@@ -0,0 +1,40 @@
+class Stationery:
+
+    def __init__(self, title: str) -> None:
+
+        self.title = title
+
+    def draw(self) -> None:
+
+        name = self.__class__.__name__
+
+        if name != 'Pencil':
+            print(f'{name}: приступил к отрисовке объекта "{self.title}"')
+        else:
+            print('Запуск отрисовки')
+            print(f'{name}: приступил к отрисовке объекта "{self.title}"')
+
+
+class Pen(Stationery):
+    pass
+
+
+class Pencil(Stationery):
+    pass
+
+
+class Handle(Stationery):
+    pass
+
+
+if __name__ == '__main__':
+    pen = Pen('Ручка')
+    pencil = Pencil('Карандаш')
+    handle = Handle('Маркер')
+    pen.draw()  # Pen: приступил к отрисовке объекта "Ручка"
+    handle.draw()  # Handle: приступил к отрисовке объекта "Маркер"
+    pencil.draw()  # Пример вывода ниже в многострочном комментарии
+    """
+    Запуск отрисовки
+    Pencil: приступил к отрисовке объекта "Карандаш"
+    """
